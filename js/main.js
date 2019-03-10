0d2e9d1b3bb43b7f62136058435af7ed658036b5
JC Sindo <digitaljoni@gmail.com>
2019-03-10 23:02:26 +0800

    rebuilding site Sun Mar 10 23:02:26 PST 2019

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ jQuery(document).ready(function(e) {
     }, 1e3)
   })
 });
+
+
+function initMobileMenu(toggle, menu) {
+  menu.classList.add('mobile-menu-closed');
+  ['click', 'touchstart'].forEach(function(eventName) {
+    toggle.addEventListener(eventName, function(evt) {
+      evt.preventDefault();
+      menu.classList.toggle('mobile-menu-open');
+      menu.classList.toggle('mobile-menu-closed');
+    });
+  });
+}
+
+var toggle = document.getElementById('mobile-menu-toggle');
+var menu = document.getElementById('main-nav-mobile');
+initMobileMenu(toggle, menu);
